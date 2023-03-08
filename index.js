969f562b2e6e53ecff3cd4d73aabfd994d29dce5
Landmark-GitHub <l.landmark.mail@gmail.com>
2023-03-08 23:31:57 +0700

    commit Add Function AddtoCash

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ var product = [{
     image: 'https://static.wixstatic.com/media/07dfe4_b5efc2f4bfdb46d7b5d1eaa6bb811e34~mv2_d_3202_4144_s_4_2.jpg/v1/fill/w_498,h_665,al_c,q_85,usm_0.66_1.00_0.01/07dfe4_b5efc2f4bfdb46d7b5d1eaa6bb811e34~mv2_d_3202_4144_s_4_2.jpg',
     name: 'GWAR',
     piece: 1500,
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat cum facilis natus corrupti itaque asperiores',
+    description: 'Lorem ipsum dolor sit amet conolor sit amet consectetolor sit amet consectetolor sit amet consolor sit amet consectetectetsectetur adipisicing elit. Fugiat cum facilis natus corrupti itaque asperiores',
     type: 'T-shit'
 },{
     id: 4,
@@ -58,7 +58,7 @@ var product = [{
 $(document).ready(() =>{
     var html = '';
     for (let i = 0; i < (product.length); i++) {
-        html += `<div onclick="openModul(${i})" class="item ${product[i].type}">
+        html += `<div onclick="openModal(${i})" class="item ${product[i].type}">
                     <img src=" ${product[i].image}">
                     <figcaption>
                         ${product[i].name}
@@ -70,12 +70,15 @@ $(document).ready(() =>{
 })
 
 var productindex = 0;
-function openModul(index) {
+function openModal(index) {
     productindex = index;
     console.log(productindex)
-    $("#modalDes").css('display','flex')
+    $("#modalDes").css('display','flex');
     $("#modalDesImage").attr('src',product[index].image)
-    $("#modalDes").attr('src',product[index].description)
+    $(".desProduct").attr('src',product[index].description)
+    $(".nameProduct").text(product[index].name)
+    $(".pieceProduct").text(numberWithCommas(product[index].piece))
+    $('.desProduct').text(product[index].description)
 }
 
 function closeModal() {
@@ -98,7 +101,7 @@ function searchSomething(key) {
     var html = '';
     for (let i = 0; i < (product.length); i++) {
         if(product[i].name.includes(valume)){
-            html += `<div class="item ${product[i].type}">
+            html += `<div onclick="openModal(${i}) class="item ${product[i].type}">
                     <img src=" ${product[i].image}">
                     <figcaption>
                         ${product[i].name}
@@ -109,4 +112,27 @@ function searchSomething(key) {
     }
 
     $("#productlist").html(html);
+}
+
+var cash = []
+function addtoCash() {
+    var pass = true
+
+    for (let i = 0; i < cash.length; i++){
+
+    }
+
+    if(pass) {
+        var obj = {
+            index: productindex,
+            id: product[productindex].id,
+            name: product[productindex].name,
+            piece: product[productindex].piece,
+            img: product[productindex].img,
+            count: 1
+        }
+        console.log(obj)
+        cash.push(obj)
+    }
+    console.log(cash)
 }
